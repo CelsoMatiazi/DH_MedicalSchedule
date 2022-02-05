68ccd4ca52704d6e6464e2f1e6532bc198482ddd
CelsoMatiazi <celsomatiazi@gmail.com>
2022-02-04 22:19:10 -0300

    login and Sign Up Screen layout

---
+++ app/src/main/java/com/matiazicelso/medicalschedule/ui/login/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.matiazicelso.medicalschedule.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.matiazicelso.medicalschedule.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        this.supportActionBar?.hide()
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.login_frag_container, SignUpFragment())
+            .commit()
+    }
+
+}
