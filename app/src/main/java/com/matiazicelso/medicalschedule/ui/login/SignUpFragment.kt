c3ad1ab078ba5c72d73a85b166784669ded6fbad
CelsoMatiazi <celsomatiazi@gmail.com>
2022-02-06 21:20:27 -0300

    ajusting code

--- app/src/main/java/com/matiazicelso/medicalschedule/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/matiazicelso/medicalschedule/ui/login/SignUpFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.matiazicelso.medicalschedule.R
 
 
-class SignUpFragment : Fragment() {
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backToLogin = view.findViewById<TextView>(R.id.signup_have_account)
+
+        backToLogin.setOnClickListener {
+            activity?.onBackPressed()
+        }
     }
+
+
 
 }
