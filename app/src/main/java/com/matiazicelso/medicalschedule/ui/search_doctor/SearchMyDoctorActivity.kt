cb2304af6b49af18a53c269549a611aa0537cb70
CelsoMatiazi <celsomatiazi@gmail.com>
2022-03-02 18:00:45 -0300

    implementando layout da search doctor

---
+++ app/src/main/java/com/matiazicelso/medicalschedule/ui/search_doctor/SearchMyDoctorActivity.kt
@@ -0,0 +1,12 @@
+package com.matiazicelso.medicalschedule.ui.search_doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.matiazicelso.medicalschedule.R
+
+class SearchMyDoctorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_my_doctor)
+    }
+}
